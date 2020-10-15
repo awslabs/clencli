7a36aeb4d698aae1a04c662f2fa5e9e95a22852a
Valter Silva <valterh@amazon.com>
2020-10-15 15:03:06 +0800

    Function that fetch command manual from yaml file

---
+++ function/man.go
@@ -0,0 +1,32 @@
+package function
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/awslabs/clencli/box"
+	yaml "gopkg.in/yaml.v2"
+)
+
+// Manual mapping the fields used by a Cobra command
+type Manual struct {
+	Use   string `yaml:"use"`
+	Short string `yaml:"short"`
+	Long  string `yaml:"long"`
+}
+
+// GetCmdManual retrieve information about the given command
+func GetCmdManual(command string) Manual {
+	var man Manual
+	manualBlob, status := box.Get("/manual/" + command + ".yaml")
+	if status {
+		err := yaml.Unmarshal(manualBlob, &man)
+		if err != nil {
+			fmt.Println("Not able to decode YAML file, error:", err)
+		}
+	} else {
+		log.Fatal("Not able to read manual from box")
+	}
+
+	return man
+}
