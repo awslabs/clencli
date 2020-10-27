ee9ee81b2e7fe3d7cc4bb2bf09fcef8ed44d103d
Valter Silva <valterh@amazon.com>
2020-10-27 16:46:32 +0800

    Add comment about functions

--- cauldron/tester.go
+++ cauldron/tester.go
@@ -10,16 +10,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// EmptyRun does TODO
 func emptyRun(*cobra.Command, []string) {}
 
-// ExecuteCommand does TODO
+// ExecuteCommand execute `root` Cobra command and return its outputs
 func ExecuteCommand(root *cobra.Command, args ...string) (output string, err error) {
 	_, output, err = executeCommandC(root, args...)
 	return output, err
 }
 
-// ExecuteCommandC does TODO
 func executeCommandC(root *cobra.Command, args ...string) (c *cobra.Command, output string, err error) {
 	buf := new(bytes.Buffer)
 	root.SetOutput(buf)
@@ -31,7 +29,7 @@ func executeCommandC(root *cobra.Command, args ...string) (c *cobra.Command, out
 	return c, buf.String(), err
 }
 
-// InitRootAndChildCmd does TODO
+// InitRootAndChildCmd initializes Cobra `root` command and add the `childCmd` to it
 func InitRootAndChildCmd(rootCmd *cobra.Command, childCmd *cobra.Command) (*cobra.Command, *cobra.Command) {
 	rootCmd.AddCommand(childCmd)
 	return rootCmd, childCmd
