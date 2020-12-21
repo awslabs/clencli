fea636e29241352fd4b7035832adabf10873b7f8
Valter Silva <valterh@amazon.com>
2020-12-21 09:40:45 +0800

    Fix profile name, and testing conditions

--- cobra/cmd/unsplash_test.go
+++ cobra/cmd/unsplash_test.go
@@ -18,10 +18,11 @@ func createUnplashCredential() {
 		var credentials model.Credentials
 
 		var profile model.CredentialProfile
-		profile.Name = "unsplash-unit-testing"
+		profile.Name = "default"
 		profile.Enabled = true // enabling profile by default
 
 		var credential model.Credential
+		credential.Name = "unit-testing"
 		credential.Enabled = true
 		credential.AccessKey = os.Getenv("UNSPLASH_ACCESS_KEY")
 		credential.SecretKey = os.Getenv("UNSPLASH_SECRET_KEY")
@@ -52,17 +53,9 @@ func TestUnsplashEmptyWithEmptyCredentials(t *testing.T) {
 }
 
 func TestUnsplashEmptyWithCredentials(t *testing.T) {
-	createUnplashCredential()
-	err := tester.ExecuteCommand(controller.UnsplashCmd(), "unsplash")
-	assert.Contains(t, err.Error(), "unable to read credentials")
-	// DeleteCredential()
-}
-
-func TestUnsplashWithUntiTestingProfile(t *testing.T) {
-	// TODO: setup the clencli/credentials before starting the test
 	pwd, nwd := tester.Setup(t)
 	createUnplashCredential()
-	err := tester.ExecuteCommand(controller.UnsplashCmd(), "unsplash", "--profile", "unit-testing")
+	err := tester.ExecuteCommand(controller.UnsplashCmd(), "unsplash")
 	dPath := pwd + "/" + nwd + "/" + "downloads"
 	assert.Nil(t, err)
 	assert.FileExists(t, "unsplash.yaml")
@@ -72,10 +65,24 @@ func TestUnsplashWithUntiTestingProfile(t *testing.T) {
 
 	files := helper.ListFiles(dPath + "/unsplash/mountains/")
 	assert.Greater(t, len(files), 0)
+
+	DeleteCredential()
 }
 
-func TestUnsplashWithQuery(t *testing.T) {
+func TestUnsplashWithUntiTestingProfile(t *testing.T) {
+	// TODO: setup the clencli/credentials before starting the test
+	pwd, nwd := tester.Setup(t)
 	createUnplashCredential()
 	err := tester.ExecuteCommand(controller.UnsplashCmd(), "unsplash", "--query", "horse")
-	assert.Contains(t, err.Error(), "invalid argument")
+	dPath := pwd + "/" + nwd + "/" + "downloads"
+	assert.Nil(t, err)
+	assert.FileExists(t, "unsplash.yaml")
+	assert.DirExists(t, dPath)
+	assert.DirExists(t, dPath+"/unsplash")
+	assert.DirExists(t, dPath+"/unsplash/horse")
+
+	files := helper.ListFiles(dPath + "/unsplash/horse/")
+	assert.Greater(t, len(files), 0)
+
+	DeleteCredential()
 }
