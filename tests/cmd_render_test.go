a2d6ae5bd54322c4d475f032640d1661a074ddbb
Valter Silva <valterh@amazon.com>
2021-01-15 14:09:39 +0800

    Move test to Unsplash

--- tests/cmd_render_test.go
+++ tests/cmd_render_test.go
@@ -1,7 +1,6 @@
 package tests
 
 import (
-	"fmt"
 	"os"
 	"testing"
 
@@ -77,53 +76,4 @@ func TestRenderHLD(t *testing.T) {
 	out, err := executeCommandOnly(t, controller.RenderCmd(), args)
 	assert.Nil(t, err)
 	assert.Contains(t, out, "Template hld.tmpl rendered as HLD.md")
-}
-
-func TestRenderUpdateLogoFromUnsplashFile(t *testing.T) {
-	createUnplashCredential()
-
-	args := []string{"init", "project", "--project-name", "foo", "--project-type", "basic"}
-	wd, out, err := executeCommandOnTemporaryDirectory(t, controller.InitCmd(), args)
-	assert.NotEmpty(t, wd)
-	assert.NotEmpty(t, out)
-	assert.Nil(t, err)
-
-	fmt.Println(wd)
-	os.Chdir("foo")
-
-	wd, _ = os.Getwd()
-
-	args = []string{"unsplash", "--query", "horse", "--size", "regular"}
-	out, err = executeCommandOnly(t, controller.UnsplashCmd(), args)
-	assert.NotEmpty(t, out)
-	assert.Nil(t, err)
-
-	args = []string{"render", "template"}
-	out, err = executeCommandOnly(t, controller.RenderCmd(), args)
-	assert.Nil(t, err)
-	assert.Contains(t, out, "Template readme.tmpl rendered as README.md")
-
-}
-
-func TestRenderUpdateLogoFromConfigurations(t *testing.T) {
-	createUnplashCredential()
-	createUnplashConfiguration()
-
-	args := []string{"init", "project", "--project-name", "foo", "--project-type", "basic"}
-	wd, out, err := executeCommandOnTemporaryDirectory(t, controller.InitCmd(), args)
-	assert.NotEmpty(t, wd)
-	assert.NotEmpty(t, out)
-	assert.Nil(t, err)
-
-	fmt.Println(wd)
-	os.Chdir("foo")
-
-	wd, _ = os.Getwd()
-	fmt.Println(wd)
-
-	args = []string{"render", "template"}
-	out, err = executeCommandOnly(t, controller.RenderCmd(), args)
-	assert.Nil(t, err)
-	assert.Contains(t, out, "Template readme.tmpl rendered as README.md")
-
 }
