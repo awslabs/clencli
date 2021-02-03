81a60313cd7f22fddcb24146fbed0203c0dcafdb
Valter Silva <valterh@amazon.com>
2021-02-03 16:33:59 +0800

    Move log flags to cmd

--- cobra/cmd/root.go
+++ cobra/cmd/root.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/awslabs/clencli/cobra/aid"
 	"github.com/awslabs/clencli/cobra/controller"
+	"github.com/awslabs/clencli/helper"
 
 	"github.com/spf13/viper"
 )
@@ -40,6 +41,10 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringP("verbosity", "v", "error", "Valid log level:panic,fatal,error,warn,info,debug,trace).")
+	rootCmd.PersistentFlags().Bool("log", true, "Enable or disable logs (can be found at ./clencli/log.json). Log outputs will be redirected default output if disabled.")
+	rootCmd.PersistentFlags().String("log-file-path", helper.BuildPath("clencli/log.json"), "Log file path. Requires log=true, ignored otherwise.")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -53,4 +58,30 @@ func initConfig() {
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("using config file:", viper.ConfigFileUsed())
 	}
+
+	verbosity, err := rootCmd.Flags().GetString("verbosity")
+	if err != nil {
+		fmt.Printf("unable to read flag verbosity\n%v", err)
+	}
+
+	log, err := rootCmd.Flags().GetBool("log")
+	if err != nil {
+		fmt.Printf("unable to read flag err\n%v", err)
+	}
+
+	logFilePath, err := rootCmd.Flags().GetString("log-file-path")
+	if err != nil {
+		fmt.Printf("unable to read flag log-file-path\n%v", err)
+	}
+
+	if log && logFilePath != "" {
+		if err := aid.SetupLoggingLevel(verbosity); err == nil {
+			fmt.Printf("logging level: %s\n", verbosity)
+		}
+
+		if err := aid.SetupLoggingOutput(logFilePath); err == nil {
+			fmt.Printf("logging path: %s\n", logFilePath)
+		}
+	}
+
 }
--- cobra/controller/root.go
+++ cobra/controller/root.go
@@ -19,20 +19,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/awslabs/clencli/cobra/aid"
 	"github.com/awslabs/clencli/helper"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var profile string
-
-//The verbose flag value
-var verbosity string
-
-var log string
-
-var logFilePath string
 
 // RootCmd represents the base command when called without any subcommands
 func RootCmd() *cobra.Command {
@@ -43,10 +34,9 @@ func RootCmd() *cobra.Command {
 	}
 
 	cmd := &cobra.Command{
-		Use:               man.Use,
-		Short:             man.Short,
-		Long:              man.Long,
-		PersistentPreRunE: rootPersistentPreRun,
+		Use:   man.Use,
+		Short: man.Short,
+		Long:  man.Long,
 	}
 
 	// Here you will define your flags and configuration settings.
@@ -55,26 +45,5 @@ func RootCmd() *cobra.Command {
 
 	// TODO: allow users to pass their prefer location for clencli's configurations directory
 
-	cmd.PersistentFlags().StringVarP(&verbosity, "verbosity", "v", logrus.ErrorLevel.String(), "Valid log level:panic,fatal,error,warn,info,debug,trace).")
-	cmd.PersistentFlags().StringVarP(&log, "log", "l", "disable", "Enable or disable logs (found at $HOME/.clencli/logs.json). Log outputs will be shown on default output.")
-	cmd.PersistentFlags().StringVar(&logFilePath, "log-file-path", aid.GetAppInfo().LogsPath, "Log file path.")
-
-	// Cobra also supports local flags, which will only run when this action is called directly.
-	cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 	return cmd
-}
-
-func rootPersistentPreRun(cmd *cobra.Command, args []string) error {
-	if err := aid.SetupLoggingLevel(verbosity); err != nil {
-		return err
-	}
-
-	if log == "enable" && logFilePath != "" {
-		if err := aid.SetupLoggingOutput(logFilePath); err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
